d4daeeed864b36c7bfb4b34a70f29e6c0ad8c2bf
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-06-21 11:06:15 -0700

    Improved CNN

--- cnn.py
+++ cnn.py
@@ -19,7 +19,7 @@ class CNN():
         self.num_epochs = num_epochs
         self.tune_lr = tune_lr
 
-    def custom_train_test_split(self, X, y, train_size):
+    def custom_train_test_split(self, X, y, train_size, val_size):
         """This function split the data at the 'train size' length. The test set 
         would be all items which index is smaller then this number, while the test 
         size all those items with an index above.
@@ -38,12 +38,16 @@ class CNN():
         # Define train sets
         X_train = X[:int(train_size*len(X))]
         y_train = y[:int(train_size*len(y))]
+        
+        # Define validations sets
+        X_val = X[int(train_size*len(X)):int(train_size*len(X) + val_size*len(X))]
+        y_val = y[int(train_size*len(y)):int(train_size*len(y) + val_size*len(y))]
 
         # Define test sets
-        X_test = X[int(train_size*len(X)):]
-        y_test = y[int(train_size*len(y)):]
+        X_test = X[int(train_size*len(X) + val_size*len(X)):]
+        y_test = y[int(train_size*len(y) + val_size*len(y)):]
 
-        return X_train, X_test, y_train, y_test
+        return X_train, X_val, X_test, y_train, y_val, y_test
 
     def reader(self):
         """This method reads the data and splits it in training and testing sets.
@@ -64,7 +68,7 @@ class CNN():
         from sklearn.preprocessing import MinMaxScaler
         scaler = MinMaxScaler()
         data.iloc[:, 1:-1] = scaler.fit_transform(data.iloc[:, 1:-1])
-        
+        print(data)
         # Convert variable columns to np.ndarray
         X = data.iloc[:, 1:-1].values
         y = data.iloc[:, -1].values
@@ -74,9 +78,75 @@ class CNN():
 
         # Split the data into test and train sets
         from sklearn.model_selection import train_test_split
-        X_train, X_test, y_train, y_test = self.custom_train_test_split(X, y, train_size=0.8)
+        X_train, X_val, X_test, y_train, y_val, y_test = self.custom_train_test_split(X, y, train_size=0.8, val_size=0.1)
 
-        return X_train, X_test, y_train, y_test, features
+        return X_train, X_val, X_test, y_train, y_val, y_test, features
+
+    def balanced_reader(self, dilation_factor):
+        """This function balances the data by keeping all the instances
+        labeled as one as those who come before and after half of the length
+        of each anomaly.
+        ----------
+        Arguments:
+        station (int): the station number.
+
+        Returns:
+        X_train: (np.array): variables train set.
+        X_test: (np.array): variables test set.
+        y_train: (np.array): target train set.
+        y_test: (np.array): target test set."""
+        
+        # Read the database
+        imbalanced_data = pd.read_csv(f'data/labeled_{self.station}.csv', sep=',', encoding='utf-8', parse_dates=['date'])
+        
+        # Normalize the data
+        from sklearn.preprocessing import MinMaxScaler
+        scaler = MinMaxScaler()
+        imbalanced_data.iloc[:, 1:-1] = scaler.fit_transform(imbalanced_data.iloc[:, 1:-1])
+
+        # Filter the data to select only rows where the label column has a value of 1
+        imbalanced_data_anomalies = imbalanced_data[imbalanced_data["label"] == 1]
+
+        # Create a new column with the difference between consecutive dates
+        date_diff = (imbalanced_data_anomalies['date'] - imbalanced_data_anomalies['date'].shift()).fillna(pd.Timedelta(minutes=15))
+
+        # Create groups of consecutive dates
+        date_group = (date_diff != pd.Timedelta(minutes=15)).cumsum()
+
+        # Get the starting and ending indexes of each group of consecutive dates
+        grouped = imbalanced_data.groupby(date_group)
+        consecutive_dates_indexes = [(group.index[0], group.index[-1]) for _, group in grouped]
+
+        balanced_data = pd.DataFrame(columns=imbalanced_data.columns)
+        for i in consecutive_dates_indexes:
+            # Get the start and end dates
+            start_date = i[0]
+            end_date = i[1]
+            
+            # Get the length of each anomaly
+            anomaly_length = end_date - start_date
+            
+            # Extract those rows labeled as anomalies with their heads and tails of zeros
+            selected_data = imbalanced_data[int(start_date - anomaly_length * dilation_factor):int(end_date + anomaly_length * dilation_factor)]
+            
+            # Fill up the balanced dataset with the selected parts
+            balanced_data = pd.concat([balanced_data, selected_data], ignore_index=True)
+        
+        # Remove those rows with missing values
+        balanced_data = balanced_data.dropna()
+        
+        # Convert variable columns to np.ndarray
+        X = balanced_data.iloc[:, 1:-1].values
+        y = balanced_data.iloc[:, -1].values
+        
+        # Get number of variables
+        features = X.shape[1]
+        
+        # Split the data into test and train sets
+        from sklearn.model_selection import train_test_split
+        X_train, X_val, X_test, y_train, y_val, y_test = self.custom_train_test_split(X, y, train_size=0.8, val_size=0.1)
+
+        return X_train, X_val, X_test, y_train, y_val, y_test, features
 
     def windows(self, array, data_type):
         """This method takes an array and generates overlapping windows.
@@ -120,14 +190,13 @@ class CNN():
     
     def plot_metrics(self, history):
         
-        metrics = ['loss', 'prc', 'precision', 'recall']
+        metrics = ['loss', 'accuracy', 'precision', 'recall', 'auc', 'prc']
         
         for n, metric in enumerate(metrics):
-            name = metric.replace("_"," ").capitalize()
-            plt.subplot(2,2,n+1)
+            name = metric.replace("_", " ").capitalize()
+            plt.subplot(2, 3, n+1)
             plt.plot(history.epoch, history.history[metric], color=colors[0], label='Train')
-            plt.plot(history.epoch, history.history['val_'+metric],
-                    color=colors[0], linestyle="--", label='Val')
+            plt.plot(history.epoch, history.history['val_'+metric], color=colors[0], linestyle="--", label='Val')
             plt.xlabel('Epoch')
             plt.ylabel(name)
             if metric == 'loss':
@@ -137,8 +206,9 @@ class CNN():
             else:
                 plt.ylim([0,1])
 
-    plt.legend()
-    def cnn(self, X_train, X_test, y_train, y_test, features, class_weight):
+            plt.legend()
+    
+    def cnn(self, X_train, X_val, X_test, y_train, y_val, y_test, features, class_weight):
         
         # Set random seed
         tf.random.set_seed(0)
@@ -148,19 +218,19 @@ class CNN():
         model = keras.models.Sequential([
             keras.layers.Conv1D(32, kernel_size=1, strides=1, input_shape=(self.group_size, features), activation='relu'),
             keras.layers.Conv1D(64, kernel_size=1, activation='relu'),
-            # keras.layers.Conv1D(128, kernel_size=1, activation='relu'),
-            # keras.layers.Conv1D(256, kernel_size=1, activation='relu'),
-            # keras.layers.Conv1D(512, kernel_size=1, activation='relu'),
+            keras.layers.Conv1D(128, kernel_size=1, activation='relu'),
+            keras.layers.Conv1D(256, kernel_size=1, activation='relu'),
+            keras.layers.Conv1D(512, kernel_size=1, activation='relu'),
             keras.layers.MaxPooling1D(pool_size=2),
-            # keras.layers.Conv1D(512, kernel_size=1, activation='relu'),
-            # keras.layers.Conv1D(1024, kernel_size=1, activation='relu'),
-            # keras.layers.Conv1D(1014, kernel_size=1, activation='relu'),
-            # keras.layers.Conv1D(512, kernel_size=1, activation='relu'),
-            # keras.layers.MaxPooling1D(pool_size=2),
+            keras.layers.Conv1D(512, kernel_size=1, activation='relu'),
+            keras.layers.Conv1D(1024, kernel_size=1, activation='relu'),
+            keras.layers.Conv1D(1014, kernel_size=1, activation='relu'),
+            keras.layers.Conv1D(512, kernel_size=1, activation='relu'),
+            keras.layers.MaxPooling1D(pool_size=2),
             keras.layers.Flatten(),
-            # keras.layers.Dense(512, activation='relu'),
-            # keras.layers.Dense(256, activation='relu'),
-            # keras.layers.Dense(128, activation='relu'),
+            keras.layers.Dense(512, activation='relu'),
+            keras.layers.Dense(256, activation='relu'),
+            keras.layers.Dense(128, activation='relu'),
             keras.layers.Dense(64, activation='relu'),
             keras.layers.Dense(32, activation='relu'),
             keras.layers.Dense(1, activation='sigmoid')
@@ -187,12 +257,10 @@ class CNN():
 
         # Train the model
         if self.tune_lr == True:
-            print("Tunning learning rate")
-            # Create a learning rate scheduler callback
-            lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-4 * 10**(epoch / 20))
-            history = model.fit(X_train, y_train, epochs=self.num_epochs, batch_size=32, callbacks=[lr_scheduler], verbose=0)
+            lr_scheduler = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 1e-4 * 10 ** (epoch / 20)) # Create a learning rate scheduler callback
+            history = model.fit(X_train, y_train, epochs=self.num_epochs, batch_size=32, validation_data=(X_val, y_val), callbacks=[lr_scheduler], verbose=0)
         else:
-            history = model.fit(X_train, y_train, epochs=self.num_epochs, batch_size=32, class_weight=class_weight, verbose=0)
+            history = model.fit(X_train, y_train, epochs=self.num_epochs, batch_size=32, validation_data=(X_val, y_val), verbose=0)
 
         # Test the performance
         results = model.evaluate(X_test, y_test, verbose=1)
@@ -201,25 +269,19 @@ class CNN():
         print()
         
         # Plot metrics
-        # self.plot_metrics(model)
-        
-        # Perform prediction and get confusion matrix
-        y_hat = model.predict(X_test)
-        
-        # Plot the loss and accuracy curves
-        pd.DataFrame(history.history).plot(figsize=(10, 7))
-        plt.title('Learning curves')
-        plt.show()
+        self.plot_metrics(history)
         
         if self.tune_lr == True:
-            print("Tunning learning rate")
-            lrs = 1e-4 * (10**(np.arange(self.num_epochs)/20))
+            lrs = 1e-4 * (10**(np.arange(self.num_epochs) / 20))
             plt.figure(figsize=(10, 7))
             plt.semilogx(lrs, history.history["loss"]) # we want the x-axis (learning rate) to be log scale
             plt.xlabel("Learning Rate")
             plt.ylabel("Loss")
             plt.title("Learning rate vs. loss")
             plt.show()
+        
+        # Perform prediction and return results
+        y_hat = model.predict(X_test)
         
         return y_hat
     
@@ -260,20 +322,23 @@ if __name__ == '__main__':
     group_size = 32
     
     # Create an instance of the CNN class
-    cnn_model = CNN(station=station, group_size=group_size, step_size=1, learning_rate=0.001, num_epochs=100, tune_lr=False)
+    cnn_model = CNN(station=station, group_size=group_size, step_size=1, learning_rate=0.0004, num_epochs=50, tune_lr=False)
 
     # Read and split the data
-    X_train, X_test, y_train, y_test, features = cnn_model.reader()
-
+    X_train, X_val, X_test, y_train, y_val, y_test, features = cnn_model.balanced_reader(dilation_factor=5)
+    # X_train, X_val, X_test, y_train, y_val, y_test, features = cnn_model.reader()
+    
     # Preprocess the data to group in overlapping windows
     X_train = cnn_model.windows(array=X_train, data_type='X')
+    X_val = cnn_model.windows(array=X_val, data_type='X')
     X_test = cnn_model.windows(array=X_test, data_type='X')
     y_train = cnn_model.windows(array=y_train, data_type='y')
+    y_val = cnn_model.windows(array=y_val, data_type='y')
     y_test = cnn_model.windows(array=y_test, data_type='y')
     
     # Calculate the class weights
     class_weight = cnn_model.class_weights(y_train)
     
-    y_hat = cnn_model.cnn(X_train, X_test, y_train, y_test, features, class_weight)
+    y_hat = cnn_model.cnn(X_train, X_val, X_test, y_train, y_val, y_test, features, class_weight)
     
     cnn_model.custom_metric(y_test, y_hat)
